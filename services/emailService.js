065f6cb7455cda80cc3590679435a987516c5ac9
Yel-lowBaby <abioyeolayinka0@gmail.com>
2026-04-15 15:47:29 +0100

    emailService updated

--- services/emailService.js
+++ services/emailService.js
@@ -3,7 +3,7 @@ const transporter = require('../configs/mailer');
 exports.sendProductEmail = async (product) => {
     await transporter.sendMail({
         from: process.env.EMAIL_USER,
-        to: "hoseatolu6@gmail.com",
+        to: "abioyeadedapo0@gmail.com",
         subject: "Inventory Manager API",
         text: `New Product Created:
         Product ${product.name} has been created successfully` 
